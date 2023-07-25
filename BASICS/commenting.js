8046935509c8b8f156fd327bcec5d6930d08aa4f
Junior Hirwa <iamhirwejr@gmail.com>
2023-07-25 08:58:41 +0200

    commenting your code in Javascript

---
+++ BASICS/commenting.js
@@ -0,0 +1,19 @@
+// 1. commenting your code in Javascript:
+
+// what are comments in Javascript?
+// coments are lines of code that Javascript will intentionally ignore
+// comments are a great way to leave notes to yourself or to other developers
+// comments are can also be used to explain what the code does
+// we have two ways to add comments in Javascript:
+// 1. single line comment
+// 2. multi line comment
+// single line comment starts with //
+// multi line comment starts with /* and ends with */
+
+// examples:
+// single line comment
+/*
+multi
+line
+comment
+*/
