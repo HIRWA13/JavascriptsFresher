a33cdb6b2b0b99a28f3ee9c412fff1192254e5d8
Junior Hirwa <iamhirwejr@gmail.com>
2023-07-25 18:19:09 +0200

    differences between var and let

---
+++ BASICS/3.varAndLet.js
@@ -0,0 +1,14 @@
+// in this file we will learn about var and let
+
+// var and let are used to declare variables in Javascript
+// var and let are keywords
+
+// differences between var and let:
+// 1. variables declared with var are function scoped.
+// 2. variables declared with let is block scoped
+// 3. variables declared with var can be re-declared
+// 4. variables declared with let cannot be re-declared
+// 5. variables declared with var can be re-assigned
+// 6. variables declared with let can be re-assigned
+// 7. variables declared with var are hoisted
+// 8. variables declare with let are not hoisted
