e2ac684c7a3a728e4586d5e956551a2d47719a3a
Junior Hirwa <iamhirwejr@gmail.com>
2023-07-25 17:49:05 +0200

    variables in Javascript

---
+++ BASICS/2.variables.js
@@ -0,0 +1,80 @@
+// Javascript Variables:
+
+// how to declare and use a variable in Javascript?
+
+// but before going into variables, let's talk about data types in Javascript:
+// what is data?
+// data is a piece of information that provides meaning to computer
+
+// what is data type?
+// data type is a classification that determines the possible values for that type
+
+// what are the data types in Javascript?
+// 1. primitive data types:
+// - number: 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+// - string: "hello world"
+// -boolean: true, false
+// -undefined: undefined
+// -null: null
+// -symbol: Symbol() -> which was introduced in ES6
+
+// 2. non-primitive data types:
+// -object: {name: "John", age: 30}
+
+// so what is a variable?
+// variable is a container for storing data values
+// we can think of variables as a box that holds data.
+// we can put data into the box and we can also get data out of the box.
+// we can also change the data inside the box.
+// we can also give the box a name so that we can refer to it later.
+
+// how to declare a variable in Javascript?
+// we can declare a variable using the keyword var, let or const
+// example:
+var name;
+let age;
+const country = null;
+
+// how to assign a value to a variable?
+// we can assign a value to a variable using the assignment operator (=)
+// example:
+name = "John";
+age = 30;
+country = "USA";
+
+// how to declare and assign a value to a variable?
+// we can declare and assign a value to a variable in one line
+// example:
+var name = "John";
+let age = 30;
+const country = "USA";
+
+// how to re-assign a value to a variable?
+// we can re-assign a value to a variable using the assignment operator (=)
+// example:
+name = "John Doe";
+age = 40;
+country = "Canada";
+
+// how to declare multiple variables in one line?
+// we can declare multiple variables in one line using the comma operator (,)
+// example:
+var name, age, country;
+let name, age, country;
+const name, age, country;
+
+// how to declare and assign multiple variables in one line?
+// we can declare and assign multiple variables in one line using the comma operator (,)
+// example:
+var name = "John", age = 30, country = "USA";
+let name = "John", age = 30, country = "USA";
+const name = "John", age = 30, country = "USA";
+
+
+
+// summary:
+// - variable is a container for storing data values
+// - we can declare a variable using the keyword var, let or const
+// - we can assign a value to a variable using the assignment operator (=)
+// - we can re-assign a value to a variable using the assignment operator (=)
+// - we can declare multiple variables in one line using the comma operator (,)
