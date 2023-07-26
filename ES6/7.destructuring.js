a2b83628fbb3a684f229bb8ab5daa8725fb09f15
Junior Hirwa <iamhirwejr@gmail.com>
2023-07-26 17:43:19 +0200

    destructuring in javascript ES6

--- ES6/7.destructuring.js
+++ ES6/7.destructuring.js
@@ -1,106 +1,127 @@
-// // Javascript ES6 Destructuring
+// // // Javascript ES6 Destructuring
 
-// // Destructuring is a convenient way of extracting multiple values from data stored in (possibly nested) objects and Arrays. It can be used in locations that receive data (such as the left-hand side of an assignment).
+// // // Destructuring is a convenient way of extracting multiple values from data stored in (possibly nested) objects and Arrays. It can be used in locations that receive data (such as the left-hand side of an assignment).
 
-let foo = ['one', 'two', 'three'];
+// let foo = ['one', 'two', 'three'];
 
-let [one, two, three] = foo;
+// let [one, two, three] = foo;
 
-console.log(two); // "two"
+// console.log(two); // "two"
 
-// example2: 
-// let a, b;
+// // example2: 
+// // let a, b;
 
-[a, b] = [1, 2];
-console.log(a); // 1
-console.log(b); // 2
-
-
-// example3: destructuring with objects
-let o = {p: 42, q: true};
-let {p, q} = o;
-
-console.log(p); // 42
+// [a, b] = [1, 2];
+// console.log(a); // 1
+// console.log(b); // 2
 
 
-// example4:
-// let a, b;
-
-({a, b} = {a: 1, b: 2});
-
-console.log(a); // 1
-console.log(b); // 2
-
-
-// example5: Assigning to new variable names
+// // example3: destructuring with objects
 // let o = {p: 42, q: true};
-// let {p: foo, q: bar} = o;
+// let {p, q} = o;
 
-console.log(foo); // 42
-console.log(bar); // true
-
-// example6: Default values
-let {a = 10, b = 5} = {a: 3};
-
-console.log(a); // 3
+// console.log(p); // 42
 
 
-//example7: use desttruncturing to assign variables from nested objects
-const user = {
-    id: 42,
-    is_verified: true
-};
+// // example4:
+// // let a, b;
 
-const {id, is_verified} = user;
+// ({a, b} = {a: 1, b: 2});
 
-console.log(id); // 42
-console.log(is_verified); // true;
+// console.log(a); // 1
+// console.log(b); // 2
 
 
-const user2 = {
-    johnDoe: {
-        age: 34,
-        email: 'Johndoe@anon.com'
-    }
-};
+// // example5: Assigning to new variable names
+// // let o = {p: 42, q: true};
+// // let {p: foo, q: bar} = o;
 
-const {johnDoe: {age, email}} = user2;
-console.log(johnDoe);// // Destruncturing is a new way of assigning values to variables from arrays and objects.
+// console.log(foo); // 42
+// console.log(bar); // true
 
-// // example: destructuring with arrays
+// // example6: Default values
+// let {a = 10, b = 5} = {a: 3};
 
-
-//destructuring assignment with the rest syntax to emulate the behavior of Array.prototype.slice(). removeFirstTwo() should return a sub-array of the original array list with the first two elements omitted.
-
-const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(a); // 3
 
 
-const removeFirstTwo = (list) => {
-    const [a, b, ...arr] = list;
-}
+// //example7: use desttruncturing to assign variables from nested objects
+// const user = {
+//     id: 42,
+//     is_verified: true
+// };
 
-const arr = removeFirstTwo(source);
+// const {id, is_verified} = user;
 
-console.log(arr); // should be [3, 4, 5, 6, 7, 8, 9, 10];
-
-// use destruncturing to pass an object as a function's parameters
-
-const stats = {
-    max: 56.78,
-    standard_deviation: 4.34,
-    median: 34.54,
-    mode: 23.87,
-    min: -0.75,
-    average: 35.85
-};
-
-const data = ({max, min}) => {
-    return (max + min) / 2.0;
-}
-
-console.log(data(stats)); // 28.015
+// console.log(id); // 42
+// console.log(is_verified); // true;
 
 
-// summary:
-// Destructuring is a new way of assigning values to variables from arrays and objects.
-// Destructuring can be used in locations that receive data (such as the left-hand side of an assignment).
+// const user2 = {
+//     johnDoe: {
+//         age: 34,
+//         email: 'Johndoe@anon.com'
+//     }
+// };
+
+// const {johnDoe: {age, email}} = user2;
+// console.log(johnDoe);// // Destruncturing is a new way of assigning values to variables from arrays and objects.
+
+// // // example: destructuring with arrays
+
+
+// //destructuring assignment with the rest syntax to emulate the behavior of Array.prototype.slice(). removeFirstTwo() should return a sub-array of the original array list with the first two elements omitted.
+
+// const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// const removeFirstTwo = (list) => {
+//     const [a, b, ...arr] = list;
+// }
+
+// const arr = removeFirstTwo(source);
+
+// console.log(arr); // should be [3, 4, 5, 6, 7, 8, 9, 10];
+
+// // use destruncturing to pass an object as a function's parameters
+
+// const stats = {
+//     max: 56.78,
+//     standard_deviation: 4.34,
+//     median: 34.54,
+//     mode: 23.87,
+//     min: -0.75,
+//     average: 35.85
+// };
+
+// const data = ({max, min}) => {
+//     return (max + min) / 2.0;
+// }
+
+// console.log(data(stats)); // 28.015
+
+
+// // summary:
+// // Destructuring is a new way of assigning values to variables from arrays and objects.
+// // Destructuring can be used in locations that receive data (such as the left-hand side of an assignment).
+
+
+// practice:
+
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F'];
+const numbers = ['1', '2', '3', '4', '5', '6'];
+
+// const a = alphabet[0];
+// const b = alphabet[1];
+
+// use destructuring to get elements a and b from the array
+const [a, b,,,...rest] = alphabet;
+console.log(a);
+console.log(b);
+
+console.log(rest);
+
+// use destructuring to get elements 1 and 2 from the array
+const [c, d,,,...rest2] = numbers;
+console.log(c);
+console.log(d);
