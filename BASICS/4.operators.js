6c4afd086b517ea9ebb09d0d408fd02b20cb5a50
Junior Hirwa <iamhirwejr@gmail.com>
2023-07-26 18:06:24 +0200

    operators in javascript

---
+++ BASICS/4.operators.js
@@ -0,0 +1,194 @@
+// Operators in Javas   
+
+// Number: the number type represents both integer and floating point numbers in javascript.
+
+// operators in javascript:
+// 1. arithmetic operators:
+// - addition: +
+// - subtraction: -
+// - multiplication: *
+// - division: /
+// - remainder: %
+// - increment: ++
+// - decrement: --
+// - exponentiation: **
+
+// 2. assignment operators:
+// - assignment: =
+// - addition assignment: +=
+// - subtraction assignment: -=
+// - multiplication assignment: *=
+// - division assignment: /=
+// - remainder assignment: %=
+// - exponentiation assignment: **=
+
+// 3. comparison operators:
+// - equal to: ==
+// - equal value and equal type: ===
+// - not equal: !=
+// - not equal value or not equal type: !==
+// - greater than: >
+// - less than: <
+// - greater than or equal to: >=
+// - less than or equal to: <=
+
+// 4. logical operators:
+// - logical AND: &&
+// - logical OR: ||
+// - logical NOT: !
+
+// 5. bitwise operators:
+// - AND: &
+// - OR: |
+// - XOR: ^
+
+// 6. string operators:
+// - concatenation: +
+
+// 7. conditional (ternary) operator:
+// - condition ? value1 : value2
+
+// 1. arithmetic operators:
+// - addition: +
+
+// example:
+var x = 5;
+var y = 10;
+var z = x + y;
+console.log(z); // 15
+
+// - subtraction: -
+
+// example:
+var x = 5;
+var y = 10;
+var z = x - y;
+
+// - multiplication: *
+
+// example:
+var x = 5;
+var y = 10;
+var z = x * y;
+
+// - division: /
+
+// example:
+var x = 5;
+var y = 10;
+var z = x / y;
+
+// - remainder: %
+
+// example:
+var x = 5;
+var y = 10;
+var z = x % y;
+
+// - increment: ++
+
+// example:
+var x = 5;
+x++;
+console.log(x); // 6
+
+// - decrement: --
+
+// example:
+var x = 5;
+x--;
+console.log(x); // 4
+
+// - exponentiation: **
+// example:
+var x = 5;
+var y = 10;
+var z = x ** y;
+console.log(z); // 9765625
+
+// 2. assignment operators:
+// - assignment: =
+
+// example:
+var x = 5;
+console.log(x); // 5
+
+// - addition assignment: +=
+// example:
+
+var x = 5;
+x += 10;
+console.log(x); // 15
+
+// - subtraction assignment: -=
+// example:
+var x = 5;
+x -= 10;
+console.log(x); // -5
+
+// - multiplication assignment: *=
+// example:
+var x = 5;
+x *= 10;
+console.log(x); // 50
+
+// - division assignment: /=
+// example:
+var x = 5;
+x /= 10;
+console.log(x); // 0.5
+
+// - remainder assignment: %=
+// example:
+var x = 5;
+x %= 10;
+console.log(x); // 5
+
+// - exponentiation assignment: **=
+// example:
+var x = 5;
+x **= 10;
+console.log(x); // 9765625
+
+// 3. comparison operators:
+// - equal to: ==
+// example:
+var x = 5;
+var y = 10;
+console.log(x == y); // false
+
+// - equal value and equal type: ===
+// example:
+var x = 5;
+var y = 10;
+console.log(x === y); // false
+
+// - not equal: !=
+// example:
+var x = 5;
+var y = 10;
+console.log(x != y); // true
+
+// - not equal value or not equal type: !==
+// example:
+var x = 5;
+var y = 10;
+console.log(x !== y); // true
+
+// - greater than: >
+// example:
+var x = 5;
+var y = 10;
+console.log(x > y); // false
+
+// - less than: <
+// example:
+var x = 5;
+var y = 10;
+console.log(x < y); // true
+
+// - greater than or equal to: >=
+// example:
+var x = 5;
+var y = 10;
+console.log(x >= y); // false
