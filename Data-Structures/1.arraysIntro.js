311bda3db6897693d2a40fddb9f36d65c613d48e
Junior Hirwa <iamhirwejr@gmail.com>
2023-07-27 17:09:09 +0200

    a hot intro to arrays in Javascript

---
+++ Data-Structures/1.arraysIntro.js
@@ -0,0 +1,20 @@
+// Arrays are variables that can hold more than one value.
+// that with arrays, we can store multiple values in a single variable.
+
+// //  example: 
+// 1. how to declare an array:
+const array = [1, 2, 3, 4, 5];
+console.log(array);
+
+// output:
+// [ 1, 2, 3, 4, 5 ]
+
+// 2. how to access an array element:
+console.log(array[0]); // 1
+console.log(array[1]); // 2
+console.log(array[2]); // 3
+
+
+
+// 3. how to change an array element:
+array[0] = 10;
