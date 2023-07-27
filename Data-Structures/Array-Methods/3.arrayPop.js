0f5966aade650724c909112c8b1962835e38db7f
Junior Hirwa <iamhirwejr@gmail.com>
2023-07-27 17:39:43 +0200

    array pop() method

---
+++ Data-Structures/Array-Methods/3.arrayPop.js
@@ -0,0 +1,18 @@
+// Javascript pop method:
+
+// the pop method is used to remove the last element from an array and it returns the value that was removed.
+// syntax:
+
+// array.pop();
+
+// example:
+// 1. remove the last element from an array:
+const array = [1, 2, 3, 4, 5];
+array.pop();
+console.log(array); // [ 1, 2, 3, 4 ]
+
+// 2. remove the last element from an array and store it in a variable:
+const array1 = [1, 2, 3, 4, 5];
+const removed = array1.pop();
+console.log(removed); // 5
+console.log(array1); // [ 1, 2, 3, 4 ]
